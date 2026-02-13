427ffbd912161496db65a0fa8205570f6f6d72d6
Sxolar1234 <sxolaranlage@gmail.com>
2026-02-13 01:46:02 +0100

    move platform finished as Prefab
    Es ist jetz ein prefab aber noch nicht ganz perfekt

--- 2dPlatformer Unity/Assets/Scripts/Platform_mov.cs
+++ 2dPlatformer Unity/Assets/Scripts/Platform_mov.cs
@@ -11,16 +11,19 @@ public class Platform_mov : MonoBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private int scoreREQ = 5;
     [SerializeField] private int monster_tax = 1;
-    [SerializeField] private bool tax_collected = false;
+    private bool tax_collected = false;
+    private bool activated = false;
 
     //public Text anzahl;
+    public Text info;
     public logicScript logic;
     public BoxCollider2D boxCollider;
+    public SpriteRenderer spriteRenderer;
 
     void Start()
     {
         startPosY = transform.position.y;
-        //anzahl = GameObject.FindWithTag("whiteMonster").GetComponent<Text>();
+        info = GameObject.FindWithTag("info").GetComponent<Text>();
         logic = GameObject.FindWithTag("logic").GetComponent<logicScript>();
     }
 
@@ -28,12 +31,18 @@ public class Platform_mov : MonoBehaviour
     {
         if (logic.getScore() >= scoreREQ)
         {
+            activated = true;
+            spriteRenderer.color = Color.green;
+        }
+        if (activated && tax_collected)
+        {
             platform_mov();
         }
     }
 
     private void platform_mov()
     {
+
         if (goingUp)
         {
             transform.Translate(Vector2.up * speed * Time.deltaTime);
@@ -48,17 +57,24 @@ public class Platform_mov : MonoBehaviour
             if (transform.position.y <= startPosY)
                 goingUp = true;
         }
+
     }
 
 
-   
 
-   public void OnCollisionEnter2D(Collision2D collision)
-   {
-      if (collision.gameObject.CompareTag("player") && !tax_collected)
-      {
-          logic.setScore(logic.getScore() - monster_tax);
-          tax_collected = true;
-      }
-   }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!tax_collected && activated && collision.gameObject.CompareTag("player"))
+        {
+            logic.setScore(logic.getScore() - monster_tax);
+            tax_collected = true;
+            spriteRenderer.color = Color.white;
+            info.text = "Platform activated! You paid the tax of " + monster_tax + " white monsters.";
+        }
+        else if (!activated && collision.gameObject.CompareTag("player"))
+        {
+            info.text = "You need to gather " + (scoreREQ - logic.getScore()) + " more white monsters to activate the platform!";
+        }
+    }
 }
